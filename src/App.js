c7d3a1800eedda7cb1d980ae77390fbad52b826d
Soliperso <chah762002@yahoo.fr>
2022-02-23 03:25:06 -0800

    add functionalities

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const STARTING_TIME = 5;
+
+  const [text, setText] = useState('');
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  const textInputRef = useRef(null);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  const calculateWordCount = (text) => {
+    const wordsArr = text.trim().split(' ');
+    return wordsArr.filter((word) => word !== '').length;
+  };
+
+  const startGame = () => {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText('');
+    textInputRef.current.disabled = false;
+    textInputRef.current.focus();
+  };
+
+  const endGame = () => {
+    setIsTimeRunning(false);
+    setWordCount(calculateWordCount(text));
+  };
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, isTimeRunning]);
+
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>Time remaining: ???</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
+      <textarea ref={textInputRef} onChange={handleChange} value={text} disabled={!isTimeRunning} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning}>
+        Start
+      </button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 };
